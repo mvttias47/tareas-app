8057ffb74f802fed06cc49c0a267ae360694ccf7
mvttias47 <matias.munoz_n@mail.udp.cl>
2025-10-19 23:31:36 -0300

    fix: ajustes en configuración de Next.js para Vercel

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
    
     ignoreDuringBuilds: true,
   },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
 };
 
 export default nextConfig;
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,7 @@
 import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 
-// postcss.config.mjs
+
 export default {
   plugins: {
     tailwindcss: {},
